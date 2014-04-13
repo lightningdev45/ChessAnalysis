715bdc162327852e89cdf9e92154632c9121049d
wpmslalom <wpmslalom@yahoo.com>
2014-04-12 23:31:36 -0400

    migration stuff

--- db/migrate/20140228155238_create_positions.rb
+++ db/migrate/20140228155238_create_positions.rb
@@ -2,7 +2,6 @@ class CreatePositions < ActiveRecord::Migration
   def change
     create_table :positions do |t|
     	t.string :fen
-    	t.text :evaluation,:hstore
       t.timestamps
     end
   end
--- db/migrate/20140308182121_removehstoreandevalfromposition.rb
+++ db/migrate/20140308182121_removehstoreandevalfromposition.rb
@@ -1,6 +1,5 @@
 class Removehstoreandevalfromposition < ActiveRecord::Migration
   def change
-  	remove_column :positions,:hstore
   	remove_column :positions,:evaluation
   end
 end
